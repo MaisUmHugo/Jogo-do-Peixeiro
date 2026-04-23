57585ff0f72cdc9d0cd77e1529bd0b5a184edbc5
Gabriel Silva Lourenço <gsl10114a@gmail.com>
2026-04-23 10:26:04 -0300

    FEAT: Começo da skybox

---
+++ Jogo-do-Peixeiro/Assets/Scripts/SkyBox/Day_Cycle.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Day_Cycle : MonoBehaviour
+{
+    [SerializeField] private Skybox Skybox;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+}
