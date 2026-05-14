6c7a760860a1fbe8b8d43c2d15d9af96413bc50c
Gabriel Silva Lourenço <gsl10114a@gmail.com>
2026-05-14 10:00:21 -0300

    FEAT: Agua Escura

--- Jogo-do-Peixeiro/Assets/Scripts/Wave/GenerateWaterMesh.cs
+++ Jogo-do-Peixeiro/Assets/Scripts/Wave/GenerateWaterMesh.cs
@@ -5,9 +5,24 @@ using UnityEngine;
 [RequireComponent(typeof(MeshCollider))]
 public class GenerateWaterMesh : MonoBehaviour
 {
-    public int resolution;
-    public float size;
+    [Header("Mesh")]
+    public int resolution = 100;
+    public float size = 100f;
+
+    [Header("Materiais")]
     public Material waterMaterial;
+    public Material darkWaterMaterial;
+
+    [Header("Referência")]
+    public Transform referenceObject;
+
+    [Header("Escurecimento")]
+    public string boatTag = "Boat";
+    public float darkStartDistance = 10f;
+    public float darkMaxDistance = 100f;
+
+    private Material runtimeDarkMaterial;
+    private Transform boat;
 
     void Awake()
     {
@@ -64,8 +79,66 @@ public class GenerateWaterMesh : MonoBehaviour
         mesh.RecalculateBounds();
 
         GetComponent<MeshFilter>().mesh = mesh;
-        GetComponent<MeshRenderer>().material = waterMaterial;
+
+        runtimeDarkMaterial = new Material(darkWaterMaterial);
+
+        GetComponent<MeshRenderer>().materials = new Material[]
+        {
+            waterMaterial,
+            runtimeDarkMaterial
+        };
 
         GetComponent<MeshCollider>().sharedMesh = mesh;
+
+        GameObject boatObject = GameObject.FindGameObjectWithTag(boatTag);
+
+        if (boatObject != null)
+        {
+            boat = boatObject.transform;
+        }
+    }
+
+    void Start()
+    {
+        if (darkWaterMaterial != null)
+        {
+            runtimeDarkMaterial = new Material(darkWaterMaterial);
+
+            runtimeDarkMaterial.SetFloat("_Surface", 1);
+            runtimeDarkMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            runtimeDarkMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+            runtimeDarkMaterial.SetInt("_ZWrite", 0);
+            runtimeDarkMaterial.DisableKeyword("_ALPHATEST_ON");
+            runtimeDarkMaterial.EnableKeyword("_ALPHABLEND_ON");
+            runtimeDarkMaterial.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            runtimeDarkMaterial.renderQueue = 3000;
+
+            GetComponent<MeshRenderer>().materials = new Material[]
+            {
+            waterMaterial,
+            runtimeDarkMaterial
+            };
+        }
+    }
+
+    void Update()
+    {
+        if (boat == null || referenceObject == null || runtimeDarkMaterial == null)
+            return;
+
+        float zDistance = boat.position.z - referenceObject.position.z;
+        Debug.Log(zDistance);
+        float darkness = Mathf.InverseLerp(
+            darkStartDistance,
+            darkMaxDistance,
+            zDistance
+        );
+
+        if (runtimeDarkMaterial.HasProperty("_BaseColor"))
+        {
+            Color baseColor = runtimeDarkMaterial.GetColor("_BaseColor");
+            baseColor.a = darkness;
+            runtimeDarkMaterial.SetColor("_BaseColor", baseColor);
+        }
     }
 }
